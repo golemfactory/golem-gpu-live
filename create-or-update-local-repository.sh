834e6a33c9acbfa7a188e866b520afea78c736d2
Frédéric Pierret (fepitre) <frederic@invisiblethingslab.com>
2023-10-04 15:45:40 +0200

    Update mirror script

--- create-or-update-local-repository.sh
+++ create-or-update-local-repository.sh
@@ -59,7 +59,7 @@ EOF
 download_deb_files \
     https://github.com/golemfactory/yagna/releases/download/pre-rel-v0.13.0-rc18/golem-provider_pre-rel-v0.13.0-rc18_amd64.deb \
     https://github.com/golemfactory/ya-runtime-wasi/releases/download/pre-rel-v0.2.4/ya-runtime-wasi-cli_0.2.3_amd64.deb \
-    https://github.com/golemfactory/ya-installer-resources/releases/download/v0.1.10/ya-installer-resources_v0.1.10.deb \
+    https://github.com/golemfactory/ya-installer-resources/releases/download/v0.1.12/ya-installer-resources_v0.1.12.deb \
     https://github.com/golemfactory/ya-runtime-vm/releases/download/pre-rel-v0.4.0-ITL-rc11/ya-runtime-vm_pre-rel-v0.4.0-ITL-rc11_amd64.deb \
     https://github.com/fepitre/golem-nvidia-kernel/releases/download/v5.10.197-2/golem-nvidia-kernel_5.10.197-2_amd64.deb
 
